ed7ce9c8112e0a6c3e5ee7daf4a51ab912d78078
samduwaji <105810325+samduwaji@users.noreply.github.com>
2022-05-22 22:39:03 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var inputss = document.getElementById("searchTerm");
+var input = document.getElementById("searchTerm");
 // Execute a function when the user releases a key on the keyboard
-inputss.addEventListener("keyup", function(event) {
+input.addEventListener("keyup", function(event) {
     // Number 13 is the "Enter" key on the keyboard
     if (event.keyCode === 13) {
         // Cancel the default action, if needed
