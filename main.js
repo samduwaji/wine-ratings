7f5457105a5b4a25fc3abd2bb3c715c245714dbd
samduwaji <105810325+samduwaji@users.noreply.github.com>
2025-11-20 16:43:46 -0800

    Update main.js

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ function searchFields (){
         {name: 'wineSpectator', link: `https://www.winespectator.com/wine/search?submitted=Y&scope=ratings&winery=${linkquery}`},
         {name: 'wineAdvocate', link: `https://www.robertparker.com/search/wine?keyword=${linkquery}`},
         {name: 'wineEnthusiast', link: `https://www.wineenthusiast.com/?s=${linkquery}&search_type=ratings`},
-        {name: 'jamesSuckling', link: `https://www.jamessuckling.com/?s=${linkquery}&content_all=All&contents%5B0%5D=Tasting+Reports&contents%5B1%5D=Tasting+Notes&contents%5B2%5D=Videos&contents%5B3%5D=Events&SelectedContents=Tasting%20Notes`},
+        {name: 'jamesSuckling', link: `https://www.jamessuckling.com/search-result?keyword=test${linkquery}`},
         {name: 'vinous', link: `https://v1.vinous.com/wines?utf8=%E2%9C%93&search-filter=reviews&query=${linkquery}`},
         {name: 'jebDunnuck', link: `https://jebdunnuck.com/wines/?keyword=${linkquery}`},
         {name: 'decanter', link: `https://www.decanter.com/wine-reviews/search?query=${linkquery}`},
@@ -74,3 +74,4 @@ function searchFields (){
         }
     }
 }
+
